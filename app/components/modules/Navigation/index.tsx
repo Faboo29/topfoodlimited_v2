bc4dab13012bfd7f1414c78b0ebaaa6d4de69d59
Fabien Liegard <fabien.liegard@ubisoft.com>
2023-07-02 15:13:48 +0200

    fix: linkedin nav icon and mobile ui

--- app/components/modules/Navigation/index.tsx
+++ app/components/modules/Navigation/index.tsx
@@ -7,17 +7,23 @@ import BorderCircle from '../../common/icons/BorderCircle';
 import styles from './navigation.module.scss';
 import { navigationItems } from '@/app/utils';
 import { kumbh } from '@/app/utils/fonts';
+import LinkedinIcon from '../../common/icons/Linkedin';
 
 const Navigation = () => {
   const [active, setActive] = useState(false);
 
   return (
     <nav id={styles.navMain} className={styles.loaded}>
-      <div
-        className={clsx(styles.navButton, active && styles.active)}
-        onClick={() => setActive((prevProps) => !prevProps)}
-      >
-        <MenuIcon />
+      <div className={styles['nav-buttons']}>
+        <a href="https://www.linkedin.com/in/vu-nguyen-topfood/" target="_blank" className={styles.contact}>
+          <LinkedinIcon />
+        </a>
+        <div
+          className={clsx(styles.navButton, active && styles.active)}
+          onClick={() => setActive((prevProps) => !prevProps)}
+        >
+          <MenuIcon />
+        </div>
       </div>
       <div className={clsx(styles.sidenavWrapper, active && styles.active, active && styles.visible)}>
         <div className={styles.sidenavWrapper__inner}>
